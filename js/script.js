df1218882b7e2fe43ad9d48c68135ed9fa9f7777
Andrew JT <andrew.julio.tompsett@gmail.com>
2021-09-02 19:28:19 +0200

    Interim Screenshot and removed duplicate text

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+- See hover states for all interactive elements on the page
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+  - The email address is not formatted correctly 
+*/
+
+const email = document.querySelector("email".value);
