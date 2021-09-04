2d888f7fe1cf069e9bda5d067fd2f4d2aea638c9
Andrew JT <andrew.julio.tompsett@gmail.com>
2021-09-04 12:44:40 +0200

    Added Email validation Logic and CSS changes

--- js/script.js
+++ js/script.js
@@ -5,4 +5,20 @@
   - The email address is not formatted correctly 
 */
 
-const email = document.querySelector("email".value);
+const email = document.querySelector(".email");
+const error = document.querySelector(".error");
+const button = document.querySelector("button");
+
+const emailValid = function (email) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+};
+
+button.addEventListener("click", function () {
+  if (email.value === "" || !emailValid(email.value)) {
+    error.style.display = "inline";
+    email.style.border = "2px solid hsl(0, 93%, 68%)";
+  } else {
+    error.style.display = "none";
+    email.style.border = "1px solid hsl(0, 6%, 24%, 20%)";
+  }
+});
